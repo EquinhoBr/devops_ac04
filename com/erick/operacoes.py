fa2111b592b377faa0cec5d1e82734c202a785bf
Erick Soares dos Santos <1900538@SALAS.AULAS>
2019-09-19 20:50:01 -0300

    Subindo arquivos 3

--- com/erick/operacoes.py
+++ com/erick/operacoes.py
@@ -1,16 +1,4 @@
 class Operacoes():
-	def soma(self, valores):
-		val = 0
-		for v in valores:
-			val = val + v
-		return val
-	
-	def subtracao(self, valores):
-		val = 0
-		for v in valores:
-			val = val - v
-		return val
-
 	def mult(self, valores):
 		val = 1
 		for v in valores:
--- com/erick/test/test_operacoes.py
+++ com/erick/test/test_operacoes.py
@@ -5,14 +5,6 @@ class TestOperacoes(TestCase):
 
 	def setUp(self):
 		self.operacoes = Operacoes()
-		
-	def test_soma(self):
-		self.assertEqual(
-			self.operacoes.soma([1,5]), 6, 'A resposta é 6')
-
-	def test_soma(self):
-		self.assertEqual(
-			self.operacoes.soma([15,10]), 25, 'A resposta é 25')
 			
 	def test_mult(self):
 		self.assertEqual(
